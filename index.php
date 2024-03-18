b0fb534b1bb3ef89df71aad9251b0c5548c1ce87
Lucas Domingues <125484200+lucasdominguess@users.noreply.github.com>
2024-03-18 19:34:57 -0300

    update project

--- index.php
+++ index.php
@@ -9,32 +9,40 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/2.0.2/css/dataTables.dataTables.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Madimi+One&family=Poppins:wght@100;400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/login.css">
+    
     <title>login</title>
 </head>
 
-<body>
+<body class="body_bg">
         
     <!-- Sessao cadastro -->
-        <section id="sessao_login" class="container-fluid">
-
+    <section id="sessao_login" class="container-fluid">
+            <div id="div-img"> <img id="img" class="opacity-50" src="img.svg" alt="">
+            </div>
             <div class="container" id="login">
 
             <form id="form_login">
                 <div class="modal-content">
                     <div>
             
-                        <div id="abacate" style="text-align: center">
+                        <div class="conteudo">
    
-                            <h3>Login</h3>  
+                            <div id="h3login">
+                                <h3 id="title_login">Login</h3> 
+                            </div> 
                             <input type="hidden" name="id" id="id">
 
                             <label for="email"><b>Email:</b></label><br>
-                            <input type="email" id="email" name="email" placeholder="exemplo@prefeitura.sp.gov"><br><br>
+                            <input class="input" type="email" id="email" name="email" placeholder="exemplo@prefeitura.sp.gov"><br><br>
 
                             <label for="senha"><b>Senha:</b></label><br>
-                            <input type="password" id="senha" name="senha"><br><br>
+                            <input class="input"  type="password" id="senha" name="senha" placeholder="Digite sua senha "><br><br>
 
-                            <button id="btn_entrar" type="button">Entrar</button>
+                            <button id="btn_entrar" class="btn_entrar" type="button">Entrar</button>
                         </div>
                     </div>
                 </div>
